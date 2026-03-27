46bb692a588861c3fea7f7c4292578bae2634ed2
Gauri Valvi <gaurivalvi27@gmail.com>
2026-03-27 15:18:14 +0000

    change in app done check done check

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("cicd working great");
+  res.send("cicd working great check");
 });
 
 app.listen(3000, () => {
